9fc363410b50ed4b64ebb637edafa531282a1365
Nataliya R <n.rudenko@gmail.com>
2019-05-13 23:54:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! I BUILT TBIS! "
+    "Started my server using Shotgun!"
   end
 
 end
